4ad98c2a0d5bf081ea96a8bfee7a9ec876cd6a20
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-04 19:41:15 +0300

    interface

--- inface/src/App.js
+++ inface/src/App.js
@@ -7,8 +7,9 @@ function App() {
       <div className="choiceFP">
         <h1 className="gamename"> Halloween </h1>
 <h2 className="gamename2"> Match-Up </h2>  
+<button className="inst">Instructions</button>
       <button className="start">Start Game</button>
-        <button className="inst">Instructions</button>
+        
         </div>
     </div>
   );
