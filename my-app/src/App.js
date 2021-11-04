d33e8d6fc380e1d4335c6d0888ceeedda31d60d6
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-04 21:14:59 +0300

    game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,50 +6,52 @@ import './App.css';
 
 function App() {
   
+  
 const [cards,setCards]=useState([
 
 
-  {
-    img:"https://www.lifewire.com/thmb/V8ZyYvb7w4pExMJsGQTDktKEtbw=/1024x768/filters:no_upscale():max_bytes(150000):strip_icc()/vladstudio_halloween_kitten_1024x768_signed-579be15f3df78c3276848237.jpg",
-  
-},
 {
-  img:"https://www.lifewire.com/thmb/V8ZyYvb7w4pExMJsGQTDktKEtbw=/1024x768/filters:no_upscale():max_bytes(150000):strip_icc()/vladstudio_halloween_kitten_1024x768_signed-579be15f3df78c3276848237.jpg",
-
-},
-
-{
-  img:"https://www.lifewire.com/thmb/V8ZyYvb7w4pExMJsGQTDktKEtbw=/1024x768/filters:no_upscale():max_bytes(150000):strip_icc()/vladstudio_halloween_kitten_1024x768_signed-579be15f3df78c3276848237.jpg",
+  img:"https://th.bing.com/th/id/R.6434510bbe4e6a19eb0a49a2d87b3ae8?rik=3QNssC%2f0Kvkkhg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-VS7hAaHjfnY%2fUmyZhbDLi2I%2fAAAAAAAAGys%2fvIRpe7OSOc4%2fs1600%2fAnimated%2bHalloween%2bWallpapers%2b5.gif&ehk=JFBhcUrKw7Omg4FwK5aSkxNohWvOWvT%2f69rjnn2SLCY%3d&risl=&pid=ImgRaw&r=0",
 
 }
 ,
 {
-  img:"https://www.lifewire.com/thmb/V8ZyYvb7w4pExMJsGQTDktKEtbw=/1024x768/filters:no_upscale():max_bytes(150000):strip_icc()/vladstudio_halloween_kitten_1024x768_signed-579be15f3df78c3276848237.jpg",
+  img:"https://th.bing.com/th/id/R.6434510bbe4e6a19eb0a49a2d87b3ae8?rik=3QNssC%2f0Kvkkhg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-VS7hAaHjfnY%2fUmyZhbDLi2I%2fAAAAAAAAGys%2fvIRpe7OSOc4%2fs1600%2fAnimated%2bHalloween%2bWallpapers%2b5.gif&ehk=JFBhcUrKw7Omg4FwK5aSkxNohWvOWvT%2f69rjnn2SLCY%3d&risl=&pid=ImgRaw&r=0"
+}
+,
+
+{
+  img:"https://th.bing.com/th/id/R.6434510bbe4e6a19eb0a49a2d87b3ae8?rik=3QNssC%2f0Kvkkhg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-VS7hAaHjfnY%2fUmyZhbDLi2I%2fAAAAAAAAGys%2fvIRpe7OSOc4%2fs1600%2fAnimated%2bHalloween%2bWallpapers%2b5.gif&ehk=JFBhcUrKw7Omg4FwK5aSkxNohWvOWvT%2f69rjnn2SLCY%3d&risl=&pid=ImgRaw&r=0",
 
 }
 ,
 
 {
-  img:"https://www.lifewire.com/thmb/V8ZyYvb7w4pExMJsGQTDktKEtbw=/1024x768/filters:no_upscale():max_bytes(150000):strip_icc()/vladstudio_halloween_kitten_1024x768_signed-579be15f3df78c3276848237.jpg",
+  img:"https://th.bing.com/th/id/R.6434510bbe4e6a19eb0a49a2d87b3ae8?rik=3QNssC%2f0Kvkkhg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-VS7hAaHjfnY%2fUmyZhbDLi2I%2fAAAAAAAAGys%2fvIRpe7OSOc4%2fs1600%2fAnimated%2bHalloween%2bWallpapers%2b5.gif&ehk=JFBhcUrKw7Omg4FwK5aSkxNohWvOWvT%2f69rjnn2SLCY%3d&risl=&pid=ImgRaw&r=0",
 
 }
 ,
 
 {
-  img:"https://www.lifewire.com/thmb/V8ZyYvb7w4pExMJsGQTDktKEtbw=/1024x768/filters:no_upscale():max_bytes(150000):strip_icc()/vladstudio_halloween_kitten_1024x768_signed-579be15f3df78c3276848237.jpg",
+  img:"https://th.bing.com/th/id/R.6434510bbe4e6a19eb0a49a2d87b3ae8?rik=3QNssC%2f0Kvkkhg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-VS7hAaHjfnY%2fUmyZhbDLi2I%2fAAAAAAAAGys%2fvIRpe7OSOc4%2fs1600%2fAnimated%2bHalloween%2bWallpapers%2b5.gif&ehk=JFBhcUrKw7Omg4FwK5aSkxNohWvOWvT%2f69rjnn2SLCY%3d&risl=&pid=ImgRaw&r=0",
 
 }
 ,
 {
-  img:"https://www.lifewire.com/thmb/V8ZyYvb7w4pExMJsGQTDktKEtbw=/1024x768/filters:no_upscale():max_bytes(150000):strip_icc()/vladstudio_halloween_kitten_1024x768_signed-579be15f3df78c3276848237.jpg",
-
-},
+  img:"https://th.bing.com/th/id/R.6434510bbe4e6a19eb0a49a2d87b3ae8?rik=3QNssC%2f0Kvkkhg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-VS7hAaHjfnY%2fUmyZhbDLi2I%2fAAAAAAAAGys%2fvIRpe7OSOc4%2fs1600%2fAnimated%2bHalloween%2bWallpapers%2b5.gif&ehk=JFBhcUrKw7Omg4FwK5aSkxNohWvOWvT%2f69rjnn2SLCY%3d&risl=&pid=ImgRaw&r=0"
+}
+,
 
 {
-  img:"https://www.lifewire.com/thmb/V8ZyYvb7w4pExMJsGQTDktKEtbw=/1024x768/filters:no_upscale():max_bytes(150000):strip_icc()/vladstudio_halloween_kitten_1024x768_signed-579be15f3df78c3276848237.jpg",
+  img:"https://th.bing.com/th/id/R.6434510bbe4e6a19eb0a49a2d87b3ae8?rik=3QNssC%2f0Kvkkhg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-VS7hAaHjfnY%2fUmyZhbDLi2I%2fAAAAAAAAGys%2fvIRpe7OSOc4%2fs1600%2fAnimated%2bHalloween%2bWallpapers%2b5.gif&ehk=JFBhcUrKw7Omg4FwK5aSkxNohWvOWvT%2f69rjnn2SLCY%3d&risl=&pid=ImgRaw&r=0",
 
-},
+}
+,
 
+{
+  img:"https://th.bing.com/th/id/R.6434510bbe4e6a19eb0a49a2d87b3ae8?rik=3QNssC%2f0Kvkkhg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-VS7hAaHjfnY%2fUmyZhbDLi2I%2fAAAAAAAAGys%2fvIRpe7OSOc4%2fs1600%2fAnimated%2bHalloween%2bWallpapers%2b5.gif&ehk=JFBhcUrKw7Omg4FwK5aSkxNohWvOWvT%2f69rjnn2SLCY%3d&risl=&pid=ImgRaw&r=0",
+
+}
 ])
 const [reverse,setReve]=useState(0)
 
@@ -64,27 +66,21 @@ setCards(whenreve)
 setReve(0)
 }
 
-
-
 console.log(cards);
 
-return (<>
- <h1> Halloween Game </h1> 
-     <div className="cards">
-  
-      
-      
-        
+return (   < div className="main">
+ 
+     <div className="cards">       
          {cards.map( (item) =>{
           return <img className="imgs" src={item.img} on /> 
         })}
-        
-        <button className="easy">Easy Level</button>
-        <button>Medium Level </button>
-        <button>High Level</button>
-    </div>
+        </div>
+        <div className="btn">
+        <button>Start Again </button>
+        <button>Next Level</button>
+        </div>
     
-  </>);
+  </div>);
 }
 
 export default App;
