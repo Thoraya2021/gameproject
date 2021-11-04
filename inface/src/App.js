b3b09ac4ea9b0a187e04237e2c160336ac3ab77d
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-04 16:42:05 +0300

    interface-pages

---
+++ inface/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <div className="choiceFP">
+        <h1 className="gamename"> Halloween </h1>
+<h2 className="gamename2"> Match-Up </h2>  
+      <button className="start">Start Game</button>
+        <button className="inst">Instructions</button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
