3bfe4afe18ec45c552bfc271c7b8fb1a908faa3d
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-05 18:34:30 +0300

    start

---
+++ start/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="mainstart">
+      <div className="startup">
+        <h1 className="gamename"> Halloween </h1>
+<h2 className="gamename2"> Match-Up </h2>  
+<div className=".choiceFP">
+       <button className="easy">easy level</button>
+      <button className="medium">medium level</button>
+      <button className="high">high level</button>
+        </div>
+        </div>
+        </div>
+    
+  );
+}
+
+export default App;
